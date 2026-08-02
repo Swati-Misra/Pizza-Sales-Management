9401abbec07839a4b6b220c6df83e0cde728a832
Swati Misra <133936056+Swati-Misra@users.noreply.github.com>
2026-08-01 20:20:11 -0400

    Add files via upload

---
+++ Intermediary SQL Query 3.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE pizzahouse;
+select * from pizzahouse.pizza_types;
+select * from pizzahouse.pizzas;
+CREATE table orders (
+order_id int not null,
+order_date date not null,
+order_time time not null,
+primary key (order_id));
+select * from orders;
+
+CREATE table order_details (
+order_details_id int not null,
+order_id int not null,
+pizza_id text not null,
+quantity int not null,
+primary key (order_details_id));
+
+select * from order_details;
+
+-- Retrieve the total number of orders placed.
+-- Join relevant tables to find the category-wise distribution of pizzas.
+ 
+SELECT pizza_types.category, count(name) FROM pizza_types
+GROUP BY pizza_types.category
+order by count(name) desc;
+ 
